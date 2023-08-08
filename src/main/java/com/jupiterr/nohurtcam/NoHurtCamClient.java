65d74fc384f42a3ae52539794edc598d300a0434
russian <dominusfinem@gmail.com>
2023-08-07 20:57:56 -0700

    init

---
+++ src/main/java/com/jupiterr/nohurtcam/NoHurtCamClient.java
@@ -0,0 +1,10 @@
+package com.jupiterr.nohurtcam;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NoHurtCamClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("bada bing");
+    }
+}
