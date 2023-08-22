3cc3e055628f89cd956a9f67eb850ff18f467732
russian <dominusfinem@gmail.com>
2023-08-21 17:56:58 -0700

    changed config and config screen

--- src/main/java/com/ruskei/nohurtcam/NoHurtCamConfig.java
+++ src/main/java/com/ruskei/nohurtcam/NoHurtCamConfig.java
@@ -1,10 +1,58 @@
 package com.ruskei.nohurtcam;
 
-import me.shedaniel.autoconfig.ConfigData;
-import me.shedaniel.autoconfig.annotation.Config;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-@Config(name = "no-hurt-cam")
-public class NoHurtCamConfig implements ConfigData {
-    public boolean shakeWorld = false;
-    public boolean shakeHand = true;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class NoHurtCamConfig {
+    public static int worldShake;
+    public static int handShake;
+    public static boolean directionalTilt;
+
+    public static void load() {
+        Path filePath = FabricLoader.getInstance().getConfigDir().resolve("no-hurt-cam.json");
+
+        try {
+            if (Files.exists(filePath)) {
+                byte[] jsonData = Files.readAllBytes(filePath);
+                JSONObject config = new JSONObject(new String(jsonData));
+
+                worldShake = config.getInt("worldShake");
+                handShake = config.getInt("handShake");
+                directionalTilt = config.getBoolean("directionalTilt");
+            } else {
+                worldShake = 0;
+                handShake = 100;
+                directionalTilt = false;
+
+                save();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void save() {
+        try {
+            JSONObject config = new JSONObject();
+            config.put("worldShake", worldShake);
+            config.put("handShake", handShake);
+            config.put("directionalTilt", directionalTilt);
+
+            Path filePath = FabricLoader.getInstance().getConfigDir().resolve("no-hurt-cam.json");
+
+            byte[] jsonData = config.toString().getBytes();
+
+            Files.write(filePath, jsonData, StandardOpenOption.CREATE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/ruskei/nohurtcam/NoHurtCamModMenu.java
+++ src/main/java/com/ruskei/nohurtcam/NoHurtCamModMenu.java
@@ -2,11 +2,47 @@ package com.ruskei.nohurtcam;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.clothconfig2.api.ConfigCategory;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
 
 public class NoHurtCamModMenu implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(NoHurtCamConfig.class, parent).get();
+        return NoHurtCamModMenu::getConfigScreen;
+    }
+
+    private static Screen getConfigScreen(Screen parent) {
+        ConfigBuilder builder = ConfigBuilder.create()
+                .setParentScreen(parent)
+                .setTitle(Text.translatable("title.no-hurt-cam.config"));
+
+        ConfigCategory general = builder.getOrCreateCategory(Text.translatable("category.no-hurt-cam.general"));
+
+        ConfigEntryBuilder entryBuilder = builder.entryBuilder();
+
+        general.addEntry(entryBuilder.startIntSlider(Text.translatable("option.no-hurt-cam.worldShake"), NoHurtCamConfig.worldShake, 0, 120)
+                .setDefaultValue(0)
+                .setTooltip(Text.of("0-120%"))
+                .setSaveConsumer(newValue -> NoHurtCamConfig.worldShake = newValue)
+                .build());
+
+        general.addEntry(entryBuilder.startIntSlider(Text.translatable("option.no-hurt-cam.handShake"), NoHurtCamConfig.handShake, 0, 120)
+                .setDefaultValue(100)
+                .setTooltip(Text.of("0-120%"))
+                .setSaveConsumer(newValue -> NoHurtCamConfig.handShake = newValue)
+                .build());
+
+        general.addEntry(entryBuilder.startBooleanToggle(Text.translatable("option.no-hurt-cam.directionalTilt"), NoHurtCamConfig.directionalTilt)
+                .setTooltip(Text.translatable("tooltip.no-hurt-cam.tiltToggle"))
+                .setSaveConsumer(newValue -> NoHurtCamConfig.directionalTilt = newValue)
+                .build());
+
+        builder.setSavingRunnable(NoHurtCamConfig::save);
+
+        parent = builder.build();
+        return parent;
     }
 }
